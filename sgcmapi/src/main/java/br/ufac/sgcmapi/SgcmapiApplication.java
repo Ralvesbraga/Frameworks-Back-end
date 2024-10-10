e2e2aa303a18e93e4a8ec22b3075d1f24787af82
Rafael Braga <ralvesbraga40@gmail.com>
2024-10-09 20:48:58 -0500

    feat: conectando o jpa/hibernate e mapeando especialidade

--- sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
@@ -1,10 +1,37 @@
 package br.ufac.sgcmapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
+@RestController
 public class SgcmapiApplication {
+
+	@Autowired
+	private ExemploService exemploService;
+	// private final ExemploService exemploService;
+
+	// public SgcmapiApplication(ExemploService exemploService){
+	// 	this.exemploService = exemploService;
+	// }
+
+	@RequestMapping(value = "/teste")
+	public String exemplo(){
+		// return "SGCM";
+		return exemploService.exibeMensagem();
+	}
+
+	@Service
+	public static class ExemploService{
+		public String exibeMensagem(){
+			return "SGCM funcionando!";
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgcmapiApplication.class, args);
